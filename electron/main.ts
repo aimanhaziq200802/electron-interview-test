e1451d430cf22da69c5acc889abe6c00a9b78b49
Aiman Haziq <muhammad.aiman@agmostudio.com>
2026-07-18 22:36:52 +0800

    feat: add main.ts and tsconfig

---
+++ electron/main.ts
@@ -0,0 +1,32 @@
+import { app, BrowserWindow } from 'electron';
+
+let mainWindow: BrowserWindow | null = null;
+
+const createWindow = () => {
+    mainWindow = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: {
+            nodeIntegration: false,
+            contextIsolation: true,
+        },
+    });
+
+    mainWindow.loadURL('http://localhost:4200');
+};
+
+app.whenReady().then(() => {
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow();
+        }
+    });
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
